76311647b98e05fb5f8483b0ba2d69b4ee3c6eea
ipaully <ipaully22@gmail.com>
2018-03-25 00:47:13 +0300

    Add comments on methods

--- app/Admin.py
+++ app/Admin.py
@@ -4,15 +4,19 @@ from app.book.book import Book
 
 
 class LibraryAdmin:
-    ''' Perfoming CRUD operations on books '''
+    ''' This class has subclasses that provide functionalities to the site administrator '''
 
     def __init__(self, user):
-        ''' Construct the library controller '''
+        ''' Initialise an instance of libraryadmin '''
         self.user = user
         self.available_books = []
+ def update_book_details(self, target_book, new_book_details):
+        ''' this method allows the admin to update book details '''
+        target_book.name, target_book.details = (new_book_details)
+        return "{} has been updated accordingly".format(target_book.name)
 
     def add_book(self, book_details):
-        ''' Functionality to create new book '''
+        ''' this enables admin to create new book '''
         book_name = book_details[0]
         if book_name in self.available_books:
             raise Exception("A book with the name {} already exists")
@@ -21,12 +25,7 @@ class LibraryAdmin:
         self.available_books.append(new_book)
         return (new_book, "{} book has been added".format(new_book))
 
-    def update_book_details(self, target_book, new_book_details):
-        ''' Functionality to update book details '''
-        target_book.name, target_book.details = (new_book_details)
-        return "{} has been updated accordingly".format(target_book.name)
-
     def delete_book(self, book):
-        ''' Functionality to delete book '''
+        ''' enables site administrator to delete book '''
         self.available_books.remove(book)
         return "VICTORY you have deleted {}".format(book)
